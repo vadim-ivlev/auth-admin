8c992be2a5bcb2a60c0f0b8082ec4f1696df5dfd
vadim-ivlev <vadim.ivlev@gmail.com>
2021-12-10 15:30:16 +0300

    fix: push to gitlab.

--- sh/push.sh
+++ sh/push.sh
@@ -10,9 +10,11 @@ echo "commit message = $msg."
 
 git commit -m "$msg."
 
+git push gitlab --all #master
 git push github --all #master
 git push origin --all #master
 
 
+git push gitlab --tags 
 git push github --tags 
 git push origin --tags 
